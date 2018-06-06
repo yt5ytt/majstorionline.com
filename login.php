4cee651b5d4c95970ef8dd4593c3eefe93c0da79
yt5ytt <yt5ytt@gmail.com>
2018-06-06 20:05:26 +0200

    Dodavanje login forme

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
               <nav>
                 <ul>
                   <li><a href="#">IDEJA SAJTA</a></li>
-                  <li><a href="login.php">PRIJAVI SE</a></li>
+                  <li><a href="index.php">POČETNA</a></li>
                   <li><a href="#">REGISTRUJ SE</a></li>
                 </ul>
               </nav>
@@ -22,26 +22,17 @@
       </div>
 
       <div class="middle-box">
-        <div class="buttons">
-          <a href="#">
-            (potreban ti je majstor)<br />
-            <h2>OBJAVI<br />
-            POSAO</h2>
-          </a>
-          <a href="#">
-            (majstori)<br />
-            <h2>PRONAĐITE<br />
-            POSAO</h2>
-          </a>
-
+        <div class="login-form">
+          <div class="login-box">
+            <h3>LOGIN</h3>
+            <form class="login-forma" action="login.php" method="post">
+              <input class="login-text" type="username" name="username" placeholder="Korisničko ime" required>
+              <input class="login-text" type="password" name="password" placeholder="Lozinka" required>
+              <input class="submit" type="submit" name="submit" value="ULOGUJ SE">
+            </form>
+          </div>
         </div>
       </div>
-
-
-
-
-
-
 <?php
   include("footer.php");
 ?>
