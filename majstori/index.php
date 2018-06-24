83d1fb515278ce7f918983a9eef9fedbf66cb96c
yt5ytt <yt5ytt@gmail.com>
2018-06-24 12:17:23 +0200

    Majstori commit

--- majstori/index.php
+++ majstori/index.php
@@ -1,8 +1,22 @@
 <?php
   session_start();
+?>
 
-  $email =  $_SESSION["email"];
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Majstori - Dashboard</title>
+  </head>
+  <body>
+    <?php
 
-  echo "Ovo je stranica za Majstora sa email adresom: " . $email;
+      $email =  $_SESSION["email"];
 
- ?>
+      echo "<h1>Ovo je stranica za Majstora sa email adresom: " . $email . "</h1>";
+      echo "<h1>MajstoriOnline<small>.com</small></h1>"
+
+     ?>
+  </body>
+</html>
